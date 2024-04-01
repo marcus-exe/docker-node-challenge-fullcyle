e96c5adc92c3185fc463a31d91fee400eec8acd4
Marcus Sena <mens.eai20@uea.edu.br>
2024-04-01 11:17:40 -0400

    correct helthcheck, simplify js code
    still dealing with some issues

--- node/app.js
+++ node/app.js
@@ -8,29 +8,18 @@ const config = {
    password: '"root"',
    database: 'nodedb'
 };
-
 const mysql = require('mysql')
+
 const connection = mysql.createConnection(config)
 
-const sql = `INSERT INTO people(name) values('Marcus')`
+const sql = `INSERT INTO people(name) values('John')`
 
-//connection.query(sql)
+connection.query(sql)
+connection.end()
 
-connection.query(sql, (err, results, fields) => {
-   if (err) {
-       console.error('Error executing query: ' + err.stack);
-       return;
-   }
-   console.log('Query executed successfully');
-   // You can process the results here if needed
-   // Remember to close the connection after processing
-   connection.end();
-});
 
-//connection.end()
-
-app.get('/', (req, res) => {
-    res.send('<h1>Full Cycle Rocks!<h1>')
+ app.get('/', (req, res) => {
+    res.send('<h1>Full Cycle<h1>')
  })
 
  app.listen(port, ()=>{
